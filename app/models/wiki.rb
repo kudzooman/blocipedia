d565cecbca395c60731935269d9eb27ec74f5d0f
vagrant <vagrant@rails-dev-box>
2014-05-27 02:19:40 +0000

    Added Premium Payment link to front page

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,10 +3,6 @@ class Wiki < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
 
-  def should_generate_new_friendly_id?
-    new_record?
-  end
-
   belongs_to :user
   
   has_many :collaborators
